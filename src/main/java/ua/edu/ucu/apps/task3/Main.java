c0cb53c7af5ea1c7c2db7ce0174c8ccf6e0dee9c
Wouch <noreply@noreply.zxc>
2024-11-15 22:44:22 +0200

    task 3

---
+++ src/main/java/ua/edu/ucu/apps/task3/Main.java
@@ -0,0 +1,7 @@
+package ua.edu.ucu.apps.task3;
+
+public class Main {
+  public static void main() {
+
+  }
+}
---
+++ src/main/java/ua/edu/ucu/apps/task3/ProxyImage.java
@@ -0,0 +1,19 @@
+package ua.edu.ucu.apps.task3;
+
+public class ProxyImage implements MyImage {
+  private RealImage realImage;
+  private String filename;
+
+  public ProxyImage(String filename) {
+    this.filename = filename;
+    this.realImage = null;
+  }
+
+  @Override
+  public void display() {
+    if (realImage == null) {
+      realImage = new RealImage(filename);
+    }
+    realImage.display();
+  }
+}
